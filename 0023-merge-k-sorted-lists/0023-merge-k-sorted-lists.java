aae2ccaf8e7305e7332e13fa8a6de0eeb82b0dc8
66brother <45537132+JunBinLiang@users.noreply.github.com>
2023-02-18 16:22:17 -0500

    Time: 20 ms (23.13%), Space: 43.4 MB (95.91%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -12,6 +12,12 @@
 // Study Java Stream with Blind75 23.Merge k Sorted Lists
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
+        /*List<Integer> list = new ArrayList<>();
+        list.add(0); list.add(1);
+        list.stream().map(i -> new ListNode(i)).forEach(n -> {
+            System.out.println(n.val);
+        });*/
+        
         return IntStream.range(0, lists.length).mapToObj(i -> Stream.iterate(lists[i], t -> t != null, t -> t.next).map(node -> node.val).collect(Collectors.toList())).flatMap(l -> l.stream()).sorted((u, v) -> v - u).map(i -> new ListNode(i)).reduce(null, (acc, cur) -> new ListNode(cur.val, acc));       
     }
 }
