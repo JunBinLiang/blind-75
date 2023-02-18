54385366e8251831e7e57a57389d632f152ab52e
66brother <45537132+JunBinLiang@users.noreply.github.com>
2023-02-18 15:24:06 -0500

    Time: 12 ms (20.04%), Space: 41.9 MB (45.46%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -14,10 +14,13 @@ class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         /*List<Integer> list = new ArrayList<>();
         list.add(1); list.add(2); list.add(3);
-        list.stream().reduce(0, (a, b) -> {
+        /*list.stream().reduce(0, (a, b) -> {
             System.out.println(a + " "  + b);
             return a + b;
         });*/
+        
+        //Stream.iterate(l1, t -> t != null, t -> t.next).mapToInt(node -> node.val).boxed().sorted((u, v) -> v - u).forEach(System.out::println);
+        
         return Stream.concat(Stream.iterate(l1, t -> t != null, t -> t.next).map(node -> node.val), Stream.iterate(l2, t -> t != null, t -> t.next).map(node -> node.val)).sorted((u, v) -> v - u).
             map(i -> new ListNode(i)).reduce(null, (acc, cur) -> new ListNode(cur.val, acc));
     }
