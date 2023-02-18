00fa9022aa10c4e8a0e2c3056e880e71e1488f5e
66brother <45537132+JunBinLiang@users.noreply.github.com>
2023-02-18 16:13:14 -0500

    Time: 19 ms (23.17%), Space: 43.2 MB (97.96%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+// Study Java Stream with Blind75 23.Merge k Sorted Lists
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        return IntStream.range(0, lists.length).mapToObj(i -> Stream.iterate(lists[i], t -> t != null, t -> t.next).map(node -> node.val).collect(Collectors.toList())).flatMap(l -> l.stream()).sorted((u, v) -> v - u).map(i -> new ListNode(i)).reduce(null, (acc, cur) -> new ListNode(cur.val, acc));       
+    }
+}
