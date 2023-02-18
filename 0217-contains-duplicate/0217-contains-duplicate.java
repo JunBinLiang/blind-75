1eae3d67b66621df1e73e1b4899d0ae5ddc4a402
66brother <45537132+JunBinLiang@users.noreply.github.com>
2023-02-18 05:34:27 -0500

    Time: 25 ms (14.77%), Space: 56.1 MB (39.22%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,7 +1,8 @@
 // Study Java Stream with Blind75 217.Contains Duplicate
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Set<Integer> s = new HashSet<>();
-        return Arrays.stream(nums).filter(x -> s.add(x)).count() != nums.length;
+       // Set<Integer> s = new HashSet<>();
+       // return Arrays.stream(nums).filter(x -> s.add(x)).count() != nums.length;
+        return Arrays.stream(nums).distinct().count() != nums.length;
     }
 }
