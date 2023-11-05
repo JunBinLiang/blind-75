5c1219e60f5f7e00a13d45d14928723ba3993368
66brother <45537132+JunBinLiang@users.noreply.github.com>
2023-11-05 03:33:20 -0500

    Time: 13 ms (9.08%), Space: 44.9 MB (97.38%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,5 +1,7 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        return Arrays.stream(intervals).sorted((o, p) -> o[0] - p[0]).reduce(new LinkedList<int[]>(), (t, u) -> (t.isEmpty() || t.getLast()[1] < u[0]) && t.add(u) || (t.getLast()[1] = Math.max(t.getLast()[1], u[1])) > 0 ? t : t, (t, u) -> t).toArray(new int[0][]);
+        /*return Arrays.stream(intervals).sorted((x, y) -> x[0] - y[0]).reduce(new LinkedList<int[]>(), (list, in) -> (((list.isEmpty() || list.getLast()[1] < in[0]) && list.add(in)) || ((list.getLast()[1] = Math.max(list.getLast()[1], in[1])) == list.getLast()[1])) ? list : list).toArray(new int[0][]);*/
+        System.out.println(Arrays.stream(intervals).sorted((x, y) -> x[0] - y[0]));
+        return Arrays.stream(intervals).sorted((x, y) -> x[0] - y[0]).reduce(new LinkedList<int[]>(), (list, in) -> (((list.isEmpty() || list.getLast()[1] < in[0]) && list.add(in)) || ((list.getLast()[1] = Math.max(list.getLast()[1], in[1])) == list.getLast()[1])) ? list : list, (list, in) -> list).toArray(new int[0][]);
     }
 }
